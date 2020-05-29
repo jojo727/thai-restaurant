57776d4a73933537107998033c07b275f6283e80
jojo727 <strawbearrymilk@protonmail.com>
2020-05-29 13:05:41 -0400

    Revised About, tiny adjustment to Main

--- src/About.js
+++ src/About.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 
-const About = () =>
+export  const About = () =>
 (
     <div className="about">
         <p>Thai Green Leaf offers delicious dining, takeout, and delivery to Copiague, NY.</p>
@@ -8,16 +8,3 @@ const About = () =>
         <p>Our Thai restaurant is known for its modern interpretation of classic dishes and its insistence on only using high quality fresh ingredients.</p>
     </div>
 )
-/*
-class About extends Component{
-render(){
-    return(
-        <div className="about">
-            <p>Thai Green Leaf offers delicious dining, takeout and delivery to Copiague, NY.</p>
-            <p>Thai Green Leaf is a cornerstone in the Copiague community and has been recognized for its outstanding Thai cuisine, excellent service and friendly staff.</p>
-            <p>Our Thai restaurant is known for its modern interpretation of classic dishes and its insistence on only using high quality fresh ingredients.</p>
-        </div>
-        );
-    }
-}*/
-export default About;
--- src/Main.js
+++ src/Main.js
@@ -7,7 +7,7 @@ import {
 import Home from "./Home";
 import Location from "./Location/Location";
 import MenuContainer from "./Menu/MenuContainer";
-import About from "./About";
+import { About } from "./About";
 import Order from "./Order/Order";
 
 class Main extends Component{
@@ -15,22 +15,31 @@ class Main extends Component{
         return(
             <HashRouter>
                 <div className="container"> 
-                    <ul className="nav-bar">
-                        <li className="left-bttn"><NavLink exact to="/">Home</NavLink></li>
-                        <li><NavLink to="/menu">Menu</NavLink></li>
-                        <li><NavLink to="/location">Location</NavLink></li>
-                        <li><NavLink to="/about">About Us</NavLink></li>
-                        <li className="right-bttn"><NavLink to="/order">Order Online</NavLink></li>
-                    </ul>
-                    <div className="break"></div>
+                    <div className="left">                  
+                    <div className="logo-container">
+                        <img id="logo" src="/assets/Thai-Green-long2.png" alt="Thai Green Leaf"/>
+                    </div></div>
+                    <div className="right"></div>
+                    <div className="nav">
+                        <ul className="nav-bar">
+                            <li className="left-bttn"><NavLink exact to="/">Home</NavLink></li>
+                            <li><NavLink to="/menu">Menu</NavLink></li>
+                            <li><NavLink to="/location">Location</NavLink></li>
+                            <li><NavLink to="/about">About Us</NavLink></li>
+                            <li className="right-bttn"><NavLink to="/order">Order Online</NavLink></li>
+                        </ul>
+                    </div>
                     <div className="content">
                         <Route exact path="/" component={Home}/>
                         <Route path="/menu" component={MenuContainer}/>
                         <Route path="/location" component={Location}/>
                         <Route path="/about" component={About}/>
                         <Route path="/order" component={Order}/>
-                    </div>
+                    </div>                
+                    <div className="footer"><p>dog</p></div>
                 </div>
+
+
             </HashRouter>
         )
     }
