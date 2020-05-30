b1e1ae5ee402f603f45bae2c3036adcfb20684cf
jojo727 <strawbearrymilk@protonmail.com>
2020-05-30 13:18:29 -0400

    Don't think there's anything new here, but I should commit this too before removing

--- src/Menu/MenuSidebar.js
+++ src/Menu/MenuSidebar.js
@@ -4,20 +4,23 @@ class MenuSidebar extends Component{
     constructor(props, any){
         super(props);
 
-        this.state={all_day: true};
+        this.state={categoryDisplayed: "all-day"};
+    }
+    componentDidUpdate(){
+        console.log(this.all_day);
     }
     render(){
-        let all_day_styles = {
-            display:(this.state.all_day ? "inline-block":"none")
-        }      
+        var all_day_styles = {
+            display:this.state.all_day ? "inline-block":"none"
+        };
+        var lunch_styles = {
+            display:this.state.all_day ? "none":"inline-block"
+        };
 
-        let lunch_styles = {
-            display:(this.state.all_day ? "none":"inline-block")
-        }
-
+    
         return(
             <div className="sidebar">
-                <ul className="all-day" style={all_day_styles}>
+                <ul className="all-day" style={this.props.isVisible}>
                     <li>Appetizers</li>
                     <li>Soups</li>
                     <li>Beverages</li>
